75ce301f4ed2d4732454ee60ff5bef8f9c7723c8
Meysonsan <meyson443@gmail.com>
2024-02-29 01:59:10 +0200

    Add abillity to throw snowballs

--- public/index.js
+++ public/index.js
@@ -13,7 +13,9 @@ const socket = io(`ws://localhost:3000/`);
 
 let groundMap = [[]];
 let players = [];
+let snowballs = [];
 const TILE_SIZE = 32;
+const SNOWBALL_RADIUS = 5;
 
 socket.on('connect', () => {
   console.log('connected');
@@ -27,6 +29,10 @@ socket.on('players', (serverPlayers) => {
     players = serverPlayers;
 });
 
+socket.on('snowballs', (serverSnowballs) => {
+    snowballs = serverSnowballs;
+});
+
 const inputs = {
     up: false,
     down: false,
@@ -36,33 +42,49 @@ const inputs = {
 
 window.addEventListener("keydown", (e) => {
     if (e.key === "w") {
-        inputs.up = true;
+        inputs["up"] = true;
     } else if (e.key === "s") {
-        inputs.down = true;
+        inputs["down"] = true;
     } else if (e.key === "d") {
-        inputs.right = true;
+        inputs["right"] = true;
     } else if (e.key === "a") {
-        inputs.left = true;
+        inputs["left"] = true;
     }
     socket.emit("input", inputs);
 });
 
 window.addEventListener("keyup", (e) => {
     if (e.key === "w") {
-        inputs.up = false;
+        inputs["up"] = false;
     } else if (e.key === "s") {
-        inputs.down = false;
+        inputs["down"] = false;
     } else if (e.key === "d") {
-        inputs.right = false;
+        inputs["right"] = false;
     } else if (e.key === "a") {
-        inputs.left = false;
+        inputs["left"] = false;
     }
     socket.emit("input", inputs);
+});
+
+window.addEventListener("click", (e) => {
+    const angle = Math.atan2(
+        e.clientY - canvasEl.height / 2, 
+        e.clientX - canvasEl.width / 2
+    );
+    socket.emit("snowball", angle);
 });
 
 function loop() {
     canvas.clearRect(0, 0, canvasEl.width, canvasEl.height);
 
+    const myPlayer = players.find((player) => player.id === socket.id);
+    let cameraX = 0;
+    let cameraY = 0;
+    if (myPlayer) {
+        cameraX = parseInt(myPlayer.x - canvasEl.width / 2);
+        cameraY = parseInt(myPlayer.y - canvasEl.height / 2);
+    }
+    
     const TILES_IN_ROW = 8;
 
     for (let row = 0; row < groundMap.length; row++) {
@@ -76,8 +98,8 @@ function loop() {
                 imageRow * TILE_SIZE,
                 TILE_SIZE,
                 TILE_SIZE,
-                col * TILE_SIZE,
-                row * TILE_SIZE,
+                col * TILE_SIZE - cameraX,
+                row * TILE_SIZE - cameraY,
                 TILE_SIZE,
                 TILE_SIZE
             );
@@ -85,8 +107,21 @@ function loop() {
     }
 
     for (const player of players) {
-        canvas.drawImage(santaImage, player.x, player.y);
+        canvas.drawImage(santaImage, player.x - cameraX, player.y - cameraY);
     };
+
+    for (const snowball of snowballs) {
+        canvas.fillStyle = "#FFFFFF";
+        canvas.beginPath();
+        canvas.arc(
+          snowball.x - cameraX,
+          snowball.y - cameraY,
+          SNOWBALL_RADIUS,
+          0,
+          2 * Math.PI
+        );
+        canvas.fill();
+      }
 
     window.requestAnimationFrame(loop);
 };
--- src/index.js
+++ src/index.js
@@ -9,12 +9,14 @@ const io = new Server(httpServer);
 const loadMap = require("./mapLoader");
 
 const SPEED = 5;
-const TICK_RATE = 30;
+const TICK_RATE = 128;
+const SNOWBALL_SPEED = 7;
 
 const players = [];
+let snowballs = [];
 const inputsMap = {};
 
-function tick() {
+function tick(delta) {
     for (const player of players) {
         const input = inputsMap[player.id];
         if (input.up) {
@@ -30,7 +32,32 @@ function tick() {
         }
     }
 
+    for (const snowball of snowballs) {
+        snowball.x += Math.cos(snowball.angle) * SNOWBALL_SPEED;
+        snowball.y += Math.sin(snowball.angle) * SNOWBALL_SPEED;
+        snowball.timeLeft -= delta;
+        // remove snowball after 1000 ticks
+        if (snowball.timeLeft <= 0) {
+            snowballs.splice(snowballs.indexOf(snowball), 1);
+        }
+
+        for (const player of players) {
+            // const distance = Math.hypot((player.x - snowball.x), (player.y - snowball.y));
+            if (player.id === snowball.playerId) continue;
+            const distance = Math.sqrt((player.x + 8 - snowball.x) ** 2 + (player.y + 8 - snowball.y) ** 2);
+            if (distance <= 8) {
+                player.x = 0;
+                player.y = 0;
+                snowball.timeLeft = -1;
+                break;
+            }
+        }
+    }
+    // remove snowball after 1000 ticks, another method
+    // snowballs = snowballs.filter((snowball) => snowball.timeLeft > 0);
+
     io.emit('players', players);
+    io.emit('snowballs', snowballs);
 }
 
 async function main() {
@@ -57,6 +84,16 @@ async function main() {
         socket.on('input', (inputs) => {
             inputsMap[socket.id] = inputs;
         });
+        socket.on('snowball', (angle) => {
+            const player =  players.find((player) => player.id === socket.id);
+            snowballs.push({
+                angle,
+                x: player.x,
+                y: player.y,
+                timeLeft: 1000,
+                playerId: socket.id
+            });
+        });
         socket.on('disconnect', () => {
             delete inputsMap[socket.id];
             players.splice(players.indexOf(socket.id), 1);
@@ -67,7 +104,15 @@ async function main() {
 
 
     httpServer.listen(3000);
-    setInterval(tick, 1000 / TICK_RATE);
+
+    let lastUpdate = Date.now();
+
+    setInterval(() => {
+        const now = Date.now();
+        const delta = now - lastUpdate;
+        tick(delta);
+        lastUpdate = now;
+      }, 1000 / TICK_RATE);
 }
 
 main();
